5aac316be3eef2bada5f2affaa38aacba9626455
GiovanniMatos <giovannyafonso23@gmail.com>
2023-11-06 07:28:49 -0400

    commit Linux

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from VALE3 import valor_vale3
 from GGBR4 import valor_ggbr4
+from DOLAR import valor_dolar
 import time
 # menor ou igual ao valor desejado
 while True:
@@ -7,4 +8,6 @@ while True:
     valor_vale3()
     print("\n\033[32m[+] GGBR4\033[0;0m")
     valor_ggbr4()
+    print("\n\033[32m[+] DÓLAR\033[0;0m")
+    valor_dolar()
     time.sleep(20)
