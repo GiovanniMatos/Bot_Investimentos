4345daaae4ff08733ff398a6308abb9be073e67c
GiovanniMatos <giovannyafonso23@gmail.com>
2023-11-08 10:09:55 -0400

    TAEE4

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from VALE3 import valor_vale3
 from GGBR4 import valor_ggbr4
 from EGIE3 import valor_egie3
-from DOLAR import valor_dolar
 from COCA34 import valor_coca34
+from TAEE4 import valor_taee4
+from DOLAR import valor_dolar
 import time
 # menor ou igual ao valor desejado
 while True:
@@ -14,6 +15,8 @@ while True:
     valor_egie3()
     print("\n\033[32m[+] COCA34\033[0;0m")
     valor_coca34()
+    print("\n\033[32m[+] TAEE4\033[0;0m")
+    valor_taee4()
     print("\n\033[32m[+] DÓLAR\033[0;0m")
     valor_dolar()
     time.sleep(20)
